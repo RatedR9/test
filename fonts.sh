977a23085d5896fd0438039ea19fb86015cb8563
RatedR9 <778408030@qq.com>
2021-04-28 12:27:46 +0800

    test

--- fonts.sh
+++ fonts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "download fonts"
-wget -O iosevka.zip https://github.com/be5invis/Iosevka/releases/download/v6.0.1/ttf-iosevka-6.0.1.zip -P /Library/A-Font && unzip /Library/A-Font/iosevka.zip
+wget  https://github.com/be5invis/Iosevka/releases/download/v6.0.1/ttf-iosevka-6.0.1.zip -P /Library/A-Font -O iosevka.zip  && unzip /Library/A-Font/iosevka.zip
 wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf -P /Library/A-Font
 rm -rf /Library/A-Font/iosevka.zip
